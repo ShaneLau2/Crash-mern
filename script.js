8eb2638ef5b4993652f2d6e69b52ee9cbcbfeb70
ShaneLau2 <xin.liu@maine.edu>
2024-04-20 21:49:44 +0100

    Update catalog colors in script.js

--- script.js
+++ script.js
@@ -5,72 +5,72 @@ const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".note-form");
 const noteList = document.querySelector(".note-list");
 
+const CATELOGIES={
+  "PROGRAMMING": "#3b82f6",
+  "LIFE":"#16a34a",
+  "UNIVERSITY":"#ef4444",
+  "MISC":"#eab308",
+  "General": "#3b82f6",
+  "Technology": "#f59e0b",
+  "Science": "#10b981",
+  "Health": "#f87171",
+  "Sport": "#f472b6",
+  "Business": "#fbbf24",
+  "Entertainment": "#f472b6",
+  "Education": "#10b981",
+  "Politics": "#f87171",
+  "Travel": "#fbbf24",
+  "Food": "#f59e0b",
+  "Other": "#3b82f6",
+};
+
 
 
 //creat DOM Elements: render initial data
-    noteList.innerHTML = '';
-// const initialData = [
-//     {
-//         "note-text": "ssssssssssssssssssssss",
-//         source: "google.com",
-//         tag: "test",
-//     },
-// ];
+noteList.innerHTML = "";
+
+
+//Load data from superbase
+getNotes();
+async function getNotes() {
+  const res = await fetch("https://emvabkeljsaitfevrbxs.supabase.co/rest/v1/notes", {
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdmFia2VsanNhaXRmZXZyYnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzOTAwMTAsImV4cCI6MjAyODk2NjAxMH0.HXToWhkhVQXdpseXYveD6N_1AVISknO-uE60iYwIDzk",
+      authorization:
+        "Bearea eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdmFia2VsanNhaXRmZXZyYnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzOTAwMTAsImV4cCI6MjAyODk2NjAxMH0.HXToWhkhVQXdpseXYveD6N_1AVISknO-uE60iYwIDzk",
+    },
+  });
+  const data = await res.json();
+console.log(data);
+creatNoteList(data);
+
+}
 
 // creatNoteList(initialData);
 
-//Load data from superbase
-const res=fetch('https://emvabkeljsaitfevrbxs.supabase.co',{
-    headers: {
-        apikey:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdmFia2VsanNhaXRmZXZyYnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzOTAwMTAsImV4cCI6MjAyODk2NjAxMH0.HXToWhkhVQXdpseXYveD6N_1AVISknO-uE60iYwIDzk
-    },
-    authorization: "Bearea eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdmFia2VsanNhaXRmZXZyYnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzOTAwMTAsImV4cCI6MjAyODk2NjAxMH0.HXToWhkhVQXdpseXYveD6N_1AVISknO-uE60iYwIDzk",
-});
-
-
 function creatNoteList(initialData) {
-const htmlArr = initialData.map(
+  const htmlArr = initialData.map(
     (note) => `<li class="note">
                         <p>${note["note-text"]}<a class="source" href="${note.source}" target="_blank">(Source)</a></p>
-                        <span class="tag" style="background-color: #3b82f6">${note.tag}</span>
+                        <span class="tag" style="background-color: ${CATELOGIES[note.tag.toUpperCase()]}">${note.tag}</span>
                         
                     </li>`
-);
-console.log(htmlArr);
-const html = htmlArr.join("");
-noteList.insertAdjacentHTML("afterbegin", html);
+  );
+  console.log(htmlArr);
+  const html = htmlArr.join("");
+  noteList.insertAdjacentHTML("afterbegin", html);
 }
 
 
+//fold and unfold the form
 btn.addEventListener("click", () => {
-    if (form.classList.contains("hidden")) {
-      form.classList.remove("hidden");
-      btn.textContent = "Close";
-    } else {
-      form.classList.add("hidden");
-      btn.textContent = "Share Something";
-    }
-  });
-
-// function creatNoteList(initialData) {
-//   const htmlArr = initialData.map(
-//     (note) => `
-//         <li class="note">
-//         <p>${note.text}<a class="source" href="${note.source}" target="_blank">(Source)</a></p>
-//         <span class="tag" style="background-color: #3b82f6"
-//             >${note.tag}</span
-//         >
-//         `
-//   );
-//   const html = htmlArr.join("");
-//   noteList.insertAdjacentHTML("afterend", html);
-// }
-
-// const test = {
-//     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, doloremque!",
-//     source: "https://www.google.com",
-//     tag: "General",
-//   };
-//   creatNoteList(test);
-
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share Something";
+  }
+});
 
